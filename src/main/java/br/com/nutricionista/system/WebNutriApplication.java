0d5e1a745a63bd3c0602648bd0ee4d5a43f23dad
hilton <hilton.carneiro@dcx.ufpb.br>
2026-04-13 14:48:22 -0300

    Initial project setup with Spring Boot base structure

---
+++ src/main/java/br/com/nutricionista/system/WebNutriApplication.java
@@ -0,0 +1,13 @@
+package br.com.nutricionista.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNutriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebNutriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/nutricionista/system/WebNutriApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nutricionista.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebNutriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
